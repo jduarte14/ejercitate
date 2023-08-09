59bdc5a9e96762c5142e7108f072e74d2ebb9d62
Joaquin Duarte <joaquinduarte1998@gmail.com>
2023-08-09 19:50:22 -0300

    Settings Page

--- ejercitate/src/components/Account.tsx
+++ ejercitate/src/components/Account.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { useRoute } from '@react-navigation/native';
-import { View, Text, ScrollView, StyleSheet, Image } from 'react-native';
+import { View, Text, ScrollView, StyleSheet, Image, Pressable } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useState, useEffect } from 'react';
+import BottomBar from './../components/bottomBar';
 
-const Account = () => {
+const Account = ({ navigation }) => {
 
     const [user, setUserInfo] = useState('');
 
@@ -37,38 +38,118 @@ const Account = () => {
     }, [])
 
     return (
-        <ScrollView>
-            <View>
+        <ScrollView style={styles.settingsContainer}>
+            <View style={styles.settingsWrapper}>
                 {user ? (
-                    <View style={styles.settingsContainer}>
-                         <Image source={{ uri: user.avatar }} style={styles.avatarImage} />
-                        <Text style={styles.text}>{user.username}</Text>
-                        <Text style={styles.text}>{user.email}</Text>
-                    </View>) :
+                    <>
+                    <View style={styles.settingsRow}>
+                        <Image source={{ uri: user.avatar }} style={styles.avatarImage} />
+                        <View style={styles.userInfo}>
+                            <Text style={styles.text}>{user.username}</Text>
+                            <Text style={styles.text}>{user.email}</Text>
+                        </View>
+                    </View>
+                    <View style={styles.buttonsContainer}>
+                    <Pressable style={styles.buttonRow}>
+                        <Image style={styles.icon} source={require('./../img/mail.png')}/>
+                            <Text style={styles.subText}>
+                                Cambiar Email
+                            </Text>
+                        </Pressable>
+                        <Pressable style={styles.buttonRow}>
+                        <Image style={styles.icon} source={require('./../img/password.png')}/>
+                            <Text style={styles.subText}>
+                                Cambiar contrasena
+                            </Text>
+                        </Pressable>
+                        <Pressable style={styles.buttonRow}>
+                        <Image style={styles.icon} source={require('./../img/avatar.png')}/>
+                            <Text style={styles.subText}>
+                                Cambiar Avatar
+                            </Text>
+                        </Pressable>
+                    </View>
+                   
+                    </>) :
                     (<Text style={styles.text}>Cargando...</Text>)}
             </View>
+            <BottomBar  navigation={navigation} />
         </ScrollView>
     )
 }
-
+let slate = "#0f172a";
 const styles = StyleSheet.create({
     settingsContainer: {
         padding: 10,
         borderRadius: 20,
         backgroundColor: "#f7f7f7",
+        flex:1,
     },
-    userInfo: {
-
+    settingsRow: {
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center"
+    },
+    icon:{
+        width:35,
+        height:35,
+    },
+    userInfo:{
+        display:"flex",
+        flexDirection:"row",
+        alignItems:"center",
+        paddingLeft:10,
+    },
+    settingsWrapper: {
+        padding: 10,
+        paddingTop:30,
+        flex:1,
     },
     text: {
-        color: "red",
+        color: slate,
         fontWeight: "bold",
         fontSize: 20,
+        paddingLeft: 10,
+        paddingTop: 10,
+        textTransform:"uppercase"
+    },
+    subText: {
+        color: slate,
+        fontWeight: "bold",
+        fontSize: 17,
+        paddingLeft: 10,
+        paddingTop: 10,
+        paddingBottom:10,
     },
     avatarImage: {
-        width: 150,
-        height: 150,
+        width: 130,
+        height: 130,
         borderRadius: 100,
+        borderWidth:4,
+        borderColor:slate,
+        elevation: 5,
+        shadowColor: '#000', 
+        shadowOffset: { width: 0, height: 2 }, 
+        shadowOpacity: 0.3, 
+        shadowRadius: 4,
+    },
+    buttonRow:{
+        backgroundColor:"white",
+        paddingVertical:15,
+        paddingLeft:10,
+        marginTop:20,
+        borderRadius:10,
+        elevation: 3,
+        shadowColor: '#000', 
+        shadowOffset: { width: 0, height: 2 }, 
+        shadowOpacity: 0.3, 
+        shadowRadius: 4,
+        display:"flex",
+        flexDirection:"row",
+        alignItems:"center",
+    },
+    buttonsContainer:{
+        padding:20,
     }
 });
 
